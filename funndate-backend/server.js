6f159a5955f3036ed04af2b576456518060c9647
vishwaradhya10 <vishwaradhyams09@gmail.com>
2025-07-28 19:59:58 +0530

    Update server for deployment

--- funndate-backend/server.js
+++ funndate-backend/server.js
@@ -4,7 +4,7 @@
     const path = require('path');
 
     const app = express();
-    const PORT = 4000;
+    const PORT = process.env.PORT || 4000;``
 
     // Middleware
     app.use(cors()); // Allows your React app to talk to this server
